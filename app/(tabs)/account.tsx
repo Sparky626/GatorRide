01dafb5aa80cebb28a758be83be5ea38f7a7654f
Geovanny Ortiz <geo.g.ortiz@gmail.com>
2025-02-02 15:35:43 -0500

    First steps laying out the ground work of the app pages

---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Account() {
+    return (
+        <View  style = {styles.container}>
+            <Text style={styles.text}>Account Info Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0b1e7d',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#eb7f05'
+    },
+});
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>GatorRide</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0b1e7d',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#eb7f05',
+  },
+});
